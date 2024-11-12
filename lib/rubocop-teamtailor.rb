a85af4ba89dd6933a5d43a864e3e9e6b77cb4efe
Jonas Brusman <jonas@brusman.se>
2024-11-12 08:50:07 +0100

    Init

---
+++ lib/rubocop-teamtailor.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'rubocop'
+
+require_relative 'rubocop/teamtailor'
+require_relative 'rubocop/teamtailor/version'
+require_relative 'rubocop/teamtailor/inject'
+
+RuboCop::Teamtailor::Inject.defaults!
+
+require_relative 'rubocop/cop/teamtailor_cops'
---
+++ spec/rubocop/teamtailor_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Rubocop::Teamtailor do
+  it "has a version number" do
+    expect(Rubocop::Teamtailor::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
